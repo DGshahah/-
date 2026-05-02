67ac7ca33adc5fcb2ace7c1c8f6995e0b27207b4
DGshahah <jh2m85mr7b@privaterelay.appleid.com>
2026-05-02 18:18:00 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+="safe5"}
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('demo').then(c=>c.addAll([
+      './','./index.html','./style.css','./app.js','./manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
